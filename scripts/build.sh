b193d93c979209987f4bea07f3e520d8b29465e6
Manas Jayanth <3097018+ManasJayanth@users.noreply.github.com>
2024-04-16 07:24:09 +0530

    Remove hardcoded msvc path

--- scripts/build.sh
+++ scripts/build.sh
@@ -4,11 +4,7 @@
 git clone https://github.com/ocaml/ocaml --recurse-submodules
 cd ocaml || exit 1
 mkdir _ocaml-prefix
-<<<<<<< HEAD
 export PATH="/usr/bin:$PATH"
-=======
-export PATH="/c/Program Files/Microsoft Visual Studio/2022/Enterprise/VC/Tools/MSVC/14.38.33130/bin/HostX64/x64:/usr/bin:$PATH"
->>>>>>> 51bb3fb (Factor out the script)
 eval "$(tools/msvs-promote-path)"
 export OCAML_PREFIX=$PWD/_ocaml-prefix
 ./configure --build=x86_64-pc-cygwin --host=x86_64-pc-windows --prefix="$OCAML_PREFIX"
